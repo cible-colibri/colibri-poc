425652f8016a9b91f2c2c218455de3b7cc1bee61
PARON <emilien.paron@cstb.fr>
2023-10-13 17:33:30 +0200

    DATAMODEL V0

--- README.md
+++ README.md
@@ -1,4 +1,58 @@
-# COLIBRI
+# COLIBRI (VF)
+ 
+Le dépot COLIBRI est constitué de deux packages principaux :
+- DATAMODEL : Ce package est destiné à la création et l'exploitation du modèle de donnée détaillé de COLIBRI associé à un set de modules défini et utilisé directement par le moteur de calcul généré.
+- MODULES : Ce package contient les modèles et méthode de calcul (au choix de l'utilisateur), ainsi que les connexions liant ces modules (ordonancement du calcul)
 
-## Description
-Mise au point du cahier des charges du nouveau cœur de calcul pour les règlementations thermiques / environnementaux du futur, pour le neuf et l’existant.
+## DATAMODEL
+Le package DATAMODEL est constitué des dossiers suivants
+- **utils** : On retrouve notamment dans ce dossier dataset_classes.py qui rassemble l'ensemble des classes génériques permettant la création ou l'édition d'un modèle de donnée COLIBRI (class DataSet() ).
+- **schemes** : On retrouve les différents schéma de données (et informations associées permettant de le consulter comme une doc via les fonctions describe() ou info() de la class DataSet) utilisés par le modèles de données COLIBRI.
+- **examples** : On y retrouve quelques exemples de jeu de donnée d'entrée au format .json, associé à un plan de situation (en png, lisible dans pycharm) et un module python permettant de générer ou éditer facilement le jeu de donnée associé en utilisant les fonctions de création et d'édition de utils/dataset_classes.py (DataSet()) 
+
+Créé une nouvelle instance DataSet de la façon suivante :
+from datamodel.utils import dataset_classes as dc
+jdd = dc.DataSet() (jdd sera utilisé dans la suite du readme)
+
+### DataSet()
+
+La classe DataSet() permet de créer et d'éditer en invite de commande un jeu de donnée COLIBRI.
+
+DataSet() contrôle que la structure du jeu de donnée fabriqué répond bien au formalisme imposé par COLIBRI tout en permettant facilement de prendre en compte la modularité de COLIBRI :
+- les propriétés des objets ne sont pas toujours les mêmes selon les modules choisis
+- il est facile de faire évoluer la structure du datamodel COLIBRI grâce au fonction génériques de DataSet et au fichier schemes du dossier "schemes"
+
+Un jeu de donnée COLIBRI est organisé autour de la structure suivante définis par 3 grandes collections :
+#### 1. "boundary_collection" : le point de départ, tout peut se rattacher et se positionner par rapport à une parois dans un bâtiment:
+Les "boundaries" (une "boundary") représentent généralement les murs, et cloisons qui cloisonnent les pièces du bâtiment.
+De façon plus générique, ce sont les frontières qui séparent des "spaces" (volumes, ex : une pièce). <br>
+A chaque boundary est associé les autres éléments (objects) constituant le bâtiment : des fenêtres, des entrées d'airs, un émetteur qui y est fixé, une chaise qui est placé sur un plancher... <br><br>
+**Tous les objects autre que boundary sont nécessairement rattachés à une boundary à travers la liste de la boundary "object_collection" (principe de base du jeu de donnée COLIBRI)**.<br><br>
+Selon si une représentation 3D du jeu de donnée est prévu dans le jeu de donnée (pas obligatoire !), ce rattachement topologique est associé à un positionnement en x,y,(z) **dans le plan de la boundary de l'élément** (positionnement relatif).
+<br>Pour comprendre la manière dont sont décrites chaque objet boundary (à ne pas confondre avec les archetype boundary, cf plus bas), n'hésitez pas à utiliser la commande dc.Boundary().describe().
+
+#### 2. "archetype_collection" : la bibliothèque des propriétés des objets :
+Si la structure des boundary et des objets qui leurs sont liés est imposée (cette structure est utilisé pour fixer les propriétés d'intégration des éléments du bâtiment).
+Les propriétés intrinsèque de chaque objet est décrite au travers d'archetypes.
+Les archetypes permettent :
+- de factoriser des propriétés communes à plusieurs objets (bibliothèque de composants) :
+<br>Ex : j'ai un type de fenetre double vitrage de dimension 80cm*180cm installée 10 fois dans mon bâtiment. Si il y aura bien 10 objets window positionnés sur différentes façades (boundary), leurs propriétés seront factorisés dans l'archetype et non répétés dans chaque window. Elles vont se contenter de pointer vers un archetype donné. 
+- de faire facilement évoluer les propriétés et caractéristiques intrinsèques demandées en fonction des modules de calculs choisis : ce sont les modules de calculs qui imposent les paramètres dont ils ont besoin pour simuler. Selon le choix des modules, alors les archetype_scheme évoluent avec les propriétés demandées par chaque module de calcul.
+<br>Pour trouver les propriétés demandées par chaque archetype, n'hésitez pas à utiliser la commande jdd.describe("nom de l'archetype") (ex : jdd.describe("layer")).
+
+#### 3. "node_collection" : ce qui lie les élements
+Cette collection rassemble les "noeuds" permettant notamment de lier les éléments matériels du bâtiment, à commencer par les boundary. Les noeuds suivants existent :
+- Les spaces : ce sont les volumes délimités par un ensemble clos de boundaries. Cela peut etre usuellement une pièce. Un space est l'unité de calcul thermique par exemple : une température par space, etc.
+- Les linear_junction : ce sont les connexions linéiques entre objets, typiquement utilisé pour lier les boundaries entre elles. Même sans interet 3D, ce sont ces linear_junction qui sont utilisés pour renseigner des ponts thermiques par exemple.
+- Les ponctual_junction : ce sont des connexions points à points entre objets. Utilisé par exemple pour relier des tuyaux entre eux.
+- Les boundary_condition : ce sont des noeuds permettant d'imposer des conditions aux limites (à une boundary, à un réseau aréaulique...)
+<br>Pour trouver les propriétés demandées par chaque archetype, n'hésitez pas à utiliser la commande jdd.describe("nom du type de noeud") (ex : jdd.describe("linear_junction")).
+
+### Schemes
+
+Dans le dossier "schemes", il existe un .py pour chaque type d'élément COLIBRI : object, archetype, node.
+Si dans le futur object_scheme et node_scheme sont bien sensé s'imposer à tous les jeu de donnée COLIBRI (et donc rester là), ce n'est pas le cas de archetype_schemes.py qui sera dépendant des modules choisis dans l'assemblage moteur de COLIBRI par chacun.
+Ainsi les schema écrits aujourd'hui dans archetype_scheme sont voués à disparaitre de "schemes" car ils seront générés à la volée comme la concaténation des paramètres demandés (avec toujours la documentation associée) pour chaque type d'objet par chaque module de calcul.
+
+## MODULES
+A faire.
---
+++ datamodel/schemes/object_schemes.py
@@ -0,0 +1,298 @@
+""" Module that provides the schema for each building object (global structure, boundary description, etc.).
+These schemas are independent of the choice of calculation modules (parameters) but determine the geometrical and topological descriptions
+of building elements. Therefore, the COLIBRI data model always retains these schemas.
+"""
+# -*- coding: utf-8 -*-
+
+# ========================================
+# External imports
+# ========================================
+
+# ========================================
+# Internal imports
+# ========================================
+
+# ========================================
+# Constants
+# ========================================
+
+# ========================================
+# Variables
+# ========================================
+
+
+# ========================================
+# Classes
+# ========================================
+
+# ========================================
+# Functions
+# ========================================
+
+# ========================================
+# Schemes
+# ========================================
+
+#region Global structure of a COLIBRI dataset
+root_scheme = {
+    "type" : "global structure",
+    "info" : "global structure of a COLIBRI json",
+    "parameters" : {},
+    "structure" : {
+        "building_land" : {},
+        "nodes_collection" : {},
+        "boundary_collection" : {},
+        "archetype_collection" : {},
+    }
+
+}
+
+#endregion
+
+#region Schema of an individual boundary inside boundary_collection
+boundary_scheme = {
+    "type" : "boundary object",
+    "info" : "A boundary is a plane surface which delimited spaces and/or exterior."
+             "\nIt has two side. It can be a fictive boundary but often represents wall, floor, roof...",
+    "parameters" : {
+        "label" : {
+            "info" : "name of the boundary (wall), not used as id",
+            "format" : "string",
+            "min" : None,
+            "max": None,
+            "unit" : None,
+            "choices" : None,
+            "default" : None
+                   },
+        "side_1": {
+            "info": "identifier of the space (or 'exterior' or 'ground' if not connected to a space or a bundary_condition node) onto which face 1 of the boundary is in contact",
+            "format" : "string",
+            "min" : None,
+            "max": None,
+            "unit" : None,
+            "choices" : None,
+            "default" : None
+                   },
+        "side_2": {
+                "info": "identifier of the space (or 'exterior' if not connected to a space) onto which face 1 of the boundary is in contact",
+                "format" : "string",
+                "min" : None,
+                "max": None,
+                "unit" : None,
+                "choices" : None,
+                "default" : None
+                    },
+        "azimuth": {
+            "info": "normal orientation of the side 1 of the boundary",
+            "format" : "int",
+            "min" : 0,
+            "max": 359,
+            "unit" : "°",
+            "choices" : None,
+            "default" : None
+        },
+        "tilt": {
+            "info": "inclination of the boundary inclination of the wall relative to the horizontal"
+                    "0° : horizontal facing upwards, 90° : vertical, 180° : horizontal facing downwards",
+            "format" : "int",
+            "min" : 0,
+            "max": 180,
+            "unit" : "°",
+            "choices" : None,
+            "default" : None
+        },
+        "area": {
+            "info": "total area of the boundary in the middle of the wall" #TODO : attention a voir si il faut side_1_area, side_2_area ou pas ou si on laisse les moteur recalculés
+                    "including the surface occupied by elements such as windows (gross area of the boundary)",
+            "format" : "float",
+            "min" : 0,
+            "max": None,
+            "unit" : "m²",
+            "choices" : None,
+            "default" : None
+        },
+        "3d_origin": {
+            "info": "Origin (x,y,z) in the absolute reference of the first point of the first segment of the boundary"
+                    "Can be used to build the 3d model without using segment information for rebuild anything", #TODO create a function that create that automatically
+            "format" : "tuple",
+            "min" : None,
+            "max": None,
+            "unit" : None,
+            "choices" : None,
+            "default" : None
+        },
+        "segments": {
+            "info": "Collection of segment forming the edges of the boundary or in case of a non 3D description linears of interest for modeling."
+                    "Important : coordinates needs to be set in CLOCKWISE order regarding side_1 of the boundary"
+                    "The coordinates of segments are set in a 2D plane (boundaries are always planar) with a relative reference point, where the first point of the boundary is designated as x:0, y:0."
+                    "In case of a non use of 3D, 'points' key is set to None and only 'lenght' is used."
+                    "Examples :[ {'id' : 'arrete_1'', 'points' : [[x1,y1],[x2,y2]], 'lenght' : 10, 'junction' : {'nodes_type' : 'linear_junction','nodes_id' : 'junction_64'} ]"
+                    "Search for 'points', 'lenght', 'junction' to know more...",
+            "format" : "list",
+            "min" : None,
+            "max": None,
+            "unit" : None,
+            "choices" : None,
+            "default" : None
+        },
+        "element_collection" : {
+            "info": "Collection of element linked to the boundary (windows, doors, inlet...)",
+            "format" : "list",
+            "min" : None,
+            "max": None,
+            "unit" : None,
+            "choices" : None,
+            "default" : []
+        },
+        "lenght" : {
+            "info": "lenght of a given segment of the boundary",
+            "format" : "float",
+            "min" : 0,
+            "max": None,
+            "unit" : "m",
+            "choices" : None,
+            "default" : None
+        },
+        "points": {
+            "info": "list of the two (x,y) points coordinates forming a segment"
+                    "ex : [[x1,y1], [x2,y2]]",
+            "format" : "list",
+            "min" : None,
+            "max": None,
+            "unit" : "Each coordinates use meter unit",
+            "choices" : None,
+            "default" : None
+        },
+        "junction" : {
+            "info": "junction is a way to connect one segment to another using 'nodes_type' et 'nodes_id' in order to find the nodes into nodes_collection of the project. It allows multiple boundary to be on contact"
+                    "ex : {'nodes_type' : 'linear_junction','nodes_id' : 'junction_64'}",
+            "format" : "dict",
+            "min" : None,
+            "max": None,
+            "unit" : None,
+            "choices" : None,
+            "default" : None
+        },
+    },
+    "structure" : {
+        "type" : "boundary",
+        "type_id" : None,
+        "label" : None,
+        "side_1": None,
+        "side_2": None,
+        "azimuth" : None,
+        "tilt" : None,
+        "area" : None,
+        "origin" : None,
+        "segments" : None, #if None we know that 2d/3d geometry is not used.
+        "object_collection" : []
+    }
+
+}
+
+#endregion
+
+#region Generic boundary object schemes
+#use for window, air_vent, various objects...
+#COMMENT : in very particular case of an object like a window who is not installated parallel to the boundary,
+# this needs to be adress in archetype with for example "additionnal orientation/additionnal tilt"
+
+boundary_object_scheme = {
+    "type" : "generic object",
+    "info" : "this scheme represents the generic scheme for all object connected to a boundary through the associated boundary key 'object_collection'\n"
+             "Each object has the same generic scheme and the list key 'junction' that allows to create other type of node connexion between dataset elements ",
+    "parameters" : {
+        "x_relative_position":{
+            "info": "relative x position of the object on the side (plane referential) of the boundary",
+            "format" : "float",
+            "min" : None,
+            "max": None,
+            "unit" : "m",
+            "choices" : None,
+            "default" : None,
+        },
+        "y_relative_position":{
+            "info": "relative y position of the object on the affected side (plane referential) of the boundary",
+            "format" : "float",
+            "min" : None,
+            "max": None,
+            "unit" : "m",
+            "choices" : None,
+            "default" : None,
+        },
+        "z_relative_position":{
+            "info": "relative z-depth position of the object compared to the affected side (plane referential) of the boundary."
+                    "\nBy default 0 but can be used when something is not really exactly on one of the two side of the boundary",
+            "format" : "float",
+            "min" : None,
+            "max": None,
+            "unit" : "m",
+            "choices" : None,
+            "default" : 0,
+        },
+        "on_side" : {
+            "info": "Parameter that allows to specify on which side of the boundary the object is located :"
+                    "\n- 'side_1',"
+                    "\n- 'side_2',"
+                    "\n- 'side_1_to_side_2' : in this case the element is connected to both face and make a hole/tunnel between them (for example : a hole, a window). The direction of integration of the object is from side 1 to side 2,"
+                    "\n- 'side_2_to_side_1' : in this case the element is connected to both face and make a hole/tunnel between them (for example : a hole, a window). The direction of integration of the object is from side 2 to side 1,",
+            "format" : "enum",
+            "min" : None,
+            "max": None,
+            "unit" : None,
+            "choices" : ["side_1","side_2","side_1_to_side_2","side_2_to_side_1"],
+            "default" : None
+        },
+        "type" : {
+            "info": "type of the object (example window) according to archetype type list",
+            "format" : "str",
+            "min" : None,
+            "max": None,
+            "unit" : None,
+            "choices" : None,
+            "default" : None,
+                  },
+        "type_id" : {
+            "info": "id of the associated archetype object which store intrinsic properties",
+            "format" : "str",
+            "min" : None,
+            "max": None,
+            "unit" : None,
+            "choices" : None,
+            "default" : None,
+                  },
+        "id" : {
+            "info": "unique id of object",
+            "format" : "str",
+            "min" : None,
+            "max": None,
+            "unit" : None,
+            "choices" : None,
+            "default" : None,
+                  },
+        "junctions" : {
+            "info": "list of node junction to which the object is connected"
+                    "example format of each element of the connexion : {'nodes_type' : 'linear_junction','nodes_id' : 'junction_64'}",
+            "format" : "list",
+            "min" : None,
+            "max": None,
+            "unit" : None,
+            "choices" : None,
+            "default" : [],
+                  },
+    },
+    "structure" : {
+        "id" : None,
+        "type" : None,
+        "type_id" : None,
+        "on_side" : None, #[side1, side2] through
+        "x_relative_position" : None,
+        "y_relative_position" : None,
+        "z_relative_position" : None,
+        "junctions" : []
+    }
+}
+
+
+
+#endregion
