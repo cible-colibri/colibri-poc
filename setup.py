ef0a20b5518615cc48e71fea028d8eda3390699e
Anthony Rey <anthony.rey@cstb.fr>
2024-05-29 12:06:02 +0200

    Update setup to add  extras_require doc

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="colibri",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
+    extras_require={
+        "doc": [
+            "sphinx",
+        ],
+    },
 )
